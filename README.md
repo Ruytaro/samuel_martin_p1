5e19d4529710b6d051269c3229d2115451c57b11
Ruytaro <9444418+Ruytaro@users.noreply.github.com>
2025-10-21 18:45:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# samuel_martin_p1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/samuel_martin_p1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.samuel_martin_p1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
