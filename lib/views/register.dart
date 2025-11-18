96edd631f9c05874ddc3d840d0fa02ce343e1bf3
Ruytaro <9444418+Ruytaro@users.noreply.github.com>
2025-11-18 19:13:45 +0100

    .

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:samuel_martin_c1/services/state_manager.dart';
 import 'package:samuel_martin_c1/services/user_manager.dart';
-import 'package:samuel_martin_c1/widgets/adminCheckBox.dart';
+import 'package:samuel_martin_c1/utils/validators.dart';
 import 'package:samuel_martin_c1/widgets/buttons.dart';
 import 'package:samuel_martin_c1/widgets/forms.dart';
 import 'package:samuel_martin_c1/utils/notifications.dart';
@@ -10,22 +10,18 @@ import 'package:samuel_martin_c1/models/user.dart';
 Widget registerView(BuildContext context) {
   UserManager um = UserManager();
   StateManager sm = StateManager();
+  final formKey = GlobalKey<FormState>();
   String name = "";
   String pass = "";
   String pass2 = "";
   String age = "";
   bool admin = false;
   void doRegister() {
-    if(pass != pass2){
-      Notifications.showError(context, "Passwords doesn't match!");
-      return;
+    if (!formKey.currentState!.validate()) {
+      Notifications.showError(context, "Review the form");
     }
     User newUser = User(name, pass);
-    newUser.age=age as int;
-    if (newUser.age < 18 || newUser.age > 100){
-
-    }
-    newUser.admin=admin;
+    newUser.admin = admin;
 
     if (um.register(newUser)) {
       sm.set("main");
@@ -37,33 +33,51 @@ Widget registerView(BuildContext context) {
   }
 
   return Center(
-    child: Column(
-      children: [
-        Image.asset("images/logo.png", width: 150),
-        myFormField((v) {
-          name = v;
-        }, "Username"),
-        myFormField(
-          (v) {
-            pass = v;
-          },
-          "Password",
-          obscure: true,
-        ),
-        myFormField(
-          (v) {
-            pass2 = v;
-          },
-          "Retype password",
-          obscure: true,
-        ),
-        myFormField((v) {
-          age = v;
-        }, "Type your age"),
-        if (um.isAdmin()) adminCheckBox(),
+    child: Form(
+      key: formKey,
+      child: Column(
+        children: [
+          Image.asset("images/logo.png", width: 150),
+          myFormField((v) {
+            name = v;
+          }, "Username"),
+          myFormField(
+            (v) {
+              pass = v;
+            },
+            "Password",
+            obscure: true,
+            validator: validateStrongPassword,
+          ),
+          TextFormField(
+            onChanged: (value) => pass2,
+            validator: (value) {
+              if (value != pass) {
+                return "Retype the password!";
+              }
+              return null;
+            },
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "Retype password",
+              border: OutlineInputBorder(),
+              constraints: BoxConstraints(maxWidth: 300),
+            ),
+          ),
+          myFormField((v) => age, "Type your age", validator: validateNumber),
+          if (um.isAdmin())
+            Center(
+              child: Row(
+                children: [
+                  Text("Give admin"),
+                  Checkbox(value: admin, onChanged: (value) => admin),
+                ],
+              ),
+            ),
 
-        myElevatedButton(doRegister, Text("Create account")),
-      ],
+          myElevatedButton(doRegister, Text("Create account")),
+        ],
+      ),
     ),
   );
 }
